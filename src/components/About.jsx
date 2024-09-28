b09c34e4505a60b3408fe97788177bf6eef53ca8
hell0world01 <arkenzyarkenstone@gmail.com>
2024-09-28 17:25:03 +0545

    just added link to my social media

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import about from '../assets/about.jpg'
 const About = () => {
   return (
-    <div className='border-b border-neutral-900 pb-4'>
+    <div className=' pb-4'>
         <h2 className='my-20 text-center text-4xl'>About <span className='text-neutral-500'>Me</span></h2>
         <div className='flex flex-wrap'>
             <div className='w-full lg:w-1/2 lg:p-8'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../assets/amritGurungLogo.png'
+import { Link } from 'react-router-dom'
 import { FaLinkedin } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaSquareXTwitter } from 'react-icons/fa6'
@@ -11,10 +12,13 @@ const Navbar = () => {
             <img src={logo} alt="" />
         </div>
         <div className='flex m-8 justify-center items-center gap-4 text-2xl'>
-            <FaLinkedin />
-            <FaGithub />
-            <FaInstagram />
-            <FaSquareXTwitter />
+          	<Link to="https://www.linkedin.com/in/amrit-gurung-4028a3272/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></Link>
+
+        	<Link to="https://github.com/hell0world01" target="_blank" rel="noopener noreferrer"><FaGithub /></Link>
+
+			<Link to="https://www.instagram.com/the.kenzy/" target="_blank" rel="noopener noreferrer"><FaInstagram /></Link>
+
+			<Link to=""><FaSquareXTwitter /></Link>
         </div>
     </nav>
   )
